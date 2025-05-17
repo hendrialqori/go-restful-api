c90b9466ce94fdae5d1779daa4114a09f3ae9bf5
Hendri Alqori <teamhendri18@gmail.com>
2025-05-17 20:49:04 +0700

    fix: fixed typo

--- service/category_service.go
+++ service/category_service.go
@@ -5,7 +5,7 @@ import (
 	"go-restful-api/model/web"
 )
 
-type Service interface {
+type CategoryService interface {
 	Create(ctx context.Context, request web.CategoryCreateRequest) web.CategoryResponse
 	Update(ctx context.Context, request web.CategoryUpdateRequest) web.CategoryResponse
 	Delete(ctx context.Context, icategoryIdd int)
